dc2754493dea1290e0418f7a24b594fdcd48685f
NicolasCornaglia <nicolascornaglia0@gmail.com>
2022-09-13 21:05:00 -0300

    ultimos cambios

--- routes/user.js
+++ routes/user.js
@@ -3,19 +3,25 @@ const router = express.Router();
 
 const userController = require('../controllers/usersController');
 const uploadFile = require('../middlewares/multerMiddleware')
-const validations = require('../middlewares/validateRegisterMiddleware');
+
+const {
+    validationsRegister,
+    validationsLogin,
+    validate
+} = require('../middlewares/validatorMiddleware');
+
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 
 // usuarios
-router.get('/login', guestMiddleware,userController.displayLogin);
+router.get('/login', guestMiddleware, userController.displayLogin);
 router.get('/register', guestMiddleware, userController.displayRegisterForm);
-router.get('/profile', authMiddleware,userController.displayProfile);
+router.get('/profile', authMiddleware, userController.displayProfile);
 router.get('/editUser', authMiddleware, userController.editView)
 
-router.post("/api/users/register", uploadFile.single("avatar"), validations, userController.processRegister);
-router.post("/api/users/login", userController.loginProcess);
+router.post("/api/users/register", validate(validationsRegister), uploadFile.single("avatar"), userController.processRegister);
+router.post("/api/users/login", validate(validationsLogin), userController.loginProcess);
 router.get("/api/users/logout", userController.logOut)
 router.put("/api/users/editUser", userController.editUser)
 
