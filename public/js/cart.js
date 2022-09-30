f86b52e72adbdc5c04002e9dd9289422b720e221
NicolasCornaglia <nicolascornaglia0@gmail.com>
2022-09-30 18:36:32 -0300

    product cart total

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,5 +1,4 @@
 
-window.onload = function () {
   function setCarritoVacio() {
     cartRows.innerHTML = `
         <tr>     
@@ -11,34 +10,56 @@ window.onload = function () {
     localStorage.removeItem("carrito");
   }
 
+  function removeItem(index) {
+    carrito.splice(index,1)
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    location.reload();
+  }
+
   function calcularTotal(products) {
     return products.reduce(
-      (acum, product) => (acum += product.price * product.quantity),
+      (acum, product) => (acum += Number(product.price) * Number(product.quantity)),
       0
     );
   }
 
   let cartRows = document.querySelector('.cartRows')
+  let products = [];
 
   if (localStorage.carrito) {
     carrito = JSON.parse(localStorage.carrito);
-
+    console.log(carrito)
     carrito.forEach((item, index) => {
       fetch(`/p/api/productCartItem/${item.id}`)
-        .then(res => { res.json() })
+        .then(res => res.json())
         .then(product => {
-          console.log(product)
-          cartRows.innerHTML += `
+          if (product) {
+            cartRows.innerHTML += `
             <tr id="row${index}">
                   <th scope="row">${index + 1}</th>
                   <td>${product.name}</td>
                   <td>${product.price}</td>
                   <td>${item.quantity}</td>
                   <td>${parseFloat(product.price * item.quantity, 2).toFixed(2)}</td>
-                  <td><button class="btn btn-warning btn-sm" onClick=removeItem(${index})><i class="fas fa-eye"></i></button></td>
+                  <td><button class="btn" onclick="removeItem(${index})">Eliminar Item</button></td>
                 </tr>
             `;
-        });
+            products.push({
+              productId: product.id,
+              nameame: product.name,
+              price: product.price,
+              quantity: item.quantity,
+            });
+          } else {
+            carrito.splice(index,1)
+            localStorage.setItem("carrito", JSON.stringify(carrito))
+          }
+        }).then( () => {
+          document.querySelector('.totalAmount').innerText = `$ ${calcularTotal(products)}`;
+        })
+
+
+
     })
   }
-}
+
