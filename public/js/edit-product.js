a699c7da3287f8379fc662b7812f6b9da43d3069
NicolasCornaglia <nicolascornaglia0@gmail.com>
2022-10-10 18:41:20 -0300

    cursor pointer en botones

---
+++ public/js/edit-product.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+   localStorage.removeItem("carrito");
+
+   // esto es provisorio ya que al ingresar a la vista de  edit product se crea 
+   // el localStorage.carrito y contiene [], hay q resolver de donde viene bien
+}
